0a5e52aad80c7b9b4f6711d204f5f201c6a13e75
Meowybot <jorgedidier149@gmail.com>
2026-01-18 12:34:43 -0600

    Update objects.lua

--- objects.lua
+++ objects.lua
@@ -1,7 +1,10 @@
 local object = {}
+love.physics.setMeter(64)
+world = love.physics.newWorld(0, 9.81*64)
 objects = {}
 
-function object.newPlayer(x, y, w, h)
+function object.newPlayer(x, y)
     local newobj = {}
-    newobj.body = nil
+    newobj.body = love.physics.newBody(world, x, y, "dynamic")
+    newobj.shape = love.physics.newRectangleShape(1, 2)
 end
